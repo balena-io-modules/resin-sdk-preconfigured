dbadb50b58df10eb703e56157bc2c532e32db7db
Tim Perry <pimterry@gmail.com>
2017-01-12 19:38:35 +0100

    Add editorconfig

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai'
 
 describe('Resin preconfigured SDK', function () {
-    this.timeout(5000);
+	this.timeout(5000);
 
-    it('can build a Resin SDK successfully', () => {
-        let sdk = require('..');
+	it('can build a Resin SDK successfully', () => {
+		let sdk = require('..');
 
-        expect(sdk).not.to.be.empty;
-    });
+		expect(sdk).not.to.be.empty;
+	});
 });
